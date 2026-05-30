d3d279d11fbb97b9a5ef5affec73da10936c8d80
ABHIGYAN ABHAY <abhigyanabhay41@gmail.com>
2026-05-30 06:44:19 +0530

    Add Multiply by 2, divide by 6 [B] from Codeforces

---
+++ 1374/B - Multiply by 2, divide by 6/solution.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+    long long n;
+        cin>>n;
+    int c=0;
+    while(n!=1){
+            if(n%6==0){
+                n=n/6;
+                c++;
+            }
+            else if(n%3==0){
+                n=n*2;
+                c++;
+            }
+            else{
+                c=-1;
+                break;
+            }
+        }
+       cout << c << endl;
+    }
+    return 0;
+}
