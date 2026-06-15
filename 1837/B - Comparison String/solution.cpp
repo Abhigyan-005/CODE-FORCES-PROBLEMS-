8bb610553a0d73a6c360b1e0a86034d203cbe2b4
ABHIGYAN ABHAY <abhigyanabhay41@gmail.com>
2026-06-15 23:37:37 +0530

    Add Comparison String [B] from Codeforces

--- 1837/B - Comparison String/solution.cpp
+++ 1837/B - Comparison String/solution.cpp
@@ -1,26 +1,150 @@
 #include <bits/stdc++.h>
- 
 using namespace std;
  
-int main()
-{
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+/*
+====================================================
+                    TYPE ALIASES
+====================================================
+*/
+ 
+using ll = long long;
+ 
+/*
+====================================================
+                    CONSTANTS
+====================================================
+*/
+ 
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
+ 
+/*
+====================================================
+                    FAST IO
+====================================================
+*/
+ 
+void fastIO(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+ 
+/*
+====================================================
+                COMMON STL CHEATSHEET
+====================================================
+ 
+VECTOR
+vector<int> v;
+v.push_back(5);
+v.size();
+ 
+PAIR
+pair<int,int> p = {1,2};
+p.first
+p.second
+ 
+SORT
+sort(v.begin(),v.end());
+sort(v.rbegin(),v.rend());
+ 
+MAP
+map<int,int> mp;
+mp[x]++;
+ 
+SET
+set<int> st;
+st.insert(x);
+ 
+====================================================
+PREFIX SUM
+====================================================
+ 
+vector<ll> pref(n+1,0);
+for(int i=1;i<=n;i++)
+    pref[i]=pref[i-1]+a[i];
+ 
+Range Sum:
+pref[r]-pref[l-1]
+ 
+====================================================
+BINARY SEARCH
+====================================================
+ 
+int l=1,r=1e9;
+while(l<=r){
+    int mid=l+(r-l)/2;
+}
+ 
+====================================================
+TWO POINTERS
+====================================================
+ 
+int l=0,r=0;
+while(r<n){
+    while(condition){
+        l++;
+    }
+    r++;
+}
+ 
+====================================================
+FREQUENCY COUNTING
+====================================================
+ 
+map<int,int> freq;
+for(int x:a)
+    freq[x]++;
+ 
+====================================================
+GREEDY
+====================================================
+ 
+1. Sort
+2. Pick best option
+3. Continue
+*/
+ 
+ll gcdll(ll a,ll b){
+    return __gcd(a,b);
+}
+ 
+ll lcmll(ll a,ll b){
+    return (a/gcdll(a,b))*b;
+}
+ 
+ll binpow(ll a,ll b){
+    ll res=1;
+    while(b){
+        if(b&1) res*=a;
+        a*=a;
+        b>>=1;
+    }
+    return res;
+}
+ 
+void solve(){
+     int n;
+     string s;
+     cin >> n >> s;
+     int ans=1, a=1;
+     for(int i=1;i<n ;i++){
+        if(s[i] == s[i-1]){
+            a++;
+        } else a=1;
+        ans = max(ans,a);
+     } cout << ans+1 << endl;
+}
+ 
+int main(){
+    fastIO();
+ 
     int t;
-    cin >> t;
-    for(int i = 0; i < t; i++)
-    {
-        int n;
-        cin >> n;
-        string s;
-        cin >> s;
-        int ans = 1, cur = 1;
-        for(int i = 1; i < n; i++)
-        {
-            if(s[i] != s[i - 1]) cur = 1;
-            else cur++;
-            ans = max(ans, cur);
-        }
-        cout << ans + 1 << endl;
-    }    
+    cin>>t;
+ 
+    while(t--){
+        solve();
+    }
+ 
+    return 0;
 }
