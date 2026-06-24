7c2d4dcad665bfc6f707e5b39754e174b7d79030
ABHIGYAN ABHAY <abhigyanabhay41@gmail.com>
2026-06-24 13:08:02 +0530

    Add Two-gram [B] from Codeforces

--- 977/B - Two-gram/solution.cpp
+++ 977/B - Two-gram/solution.cpp
@@ -1,30 +1,158 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
  
-int main() {
-    int n;
-    string s;
-    cin >> n >> s;
+/*
+====================================================
+                    TYPE ALIASES
+====================================================
+*/
  
-    string ans = "";
-    int mx = 0;
+using ll = long long;
  
-    for (int i = 0; i < n - 1; i++) {
-        int cnt = 0;
+/*
+====================================================
+                    CONSTANTS
+====================================================
+*/
  
-        for (int j = 0; j < n - 1; j++) {
-            if (s[i] == s[j] && s[i + 1] == s[j + 1]) {
-                cnt++;
-            }
-        }
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
  
-        if (cnt > mx) {
-            mx = cnt;
-            ans = "";
-            ans += s[i];
-            ans += s[i + 1];
-        }
+/*
+====================================================
+                    FAST IO
+====================================================
+*/
+ 
+void fastIO(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+}
+ 
+/*
+====================================================
+                COMMON STL CHEATSHEET
+====================================================
+ 
+VECTOR
+vector<int> v;
+v.push_back(5);
+v.size();
+ 
+PAIR
+pair<int,int> p = {1,2};
+p.first
+p.second
+ 
+SORT
+sort(v.begin(),v.end());
+sort(v.rbegin(),v.rend());
+ 
+MAP
+map<int,int> mp;
+mp[x]++;
+ 
+SET
+set<int> st;
+st.insert(x);
+ 
+====================================================
+PREFIX SUM
+====================================================
+ 
+vector<ll> pref(n+1,0);
+for(int i=1;i<=n;i++)
+    pref[i]=pref[i-1]+a[i];
+ 
+Range Sum:
+pref[r]-pref[l-1]
+ 
+====================================================
+BINARY SEARCH
+====================================================
+ 
+int l=1,r=1e9;
+while(l<=r){
+    int mid=l+(r-l)/2;
+}
+ 
+====================================================
+TWO POINTERS
+====================================================
+ 
+int l=0,r=0;
+while(r<n){
+    while(condition){
+        l++;
     }
+    r++;
+}
  
-    cout << ans;
+====================================================
+FREQUENCY COUNTING
+====================================================
+ 
+map<int,int> freq;
+for(int x:a)
+    freq[x]++;
+ 
+====================================================
+GREEDY
+====================================================
+ 
+1. Sort
+2. Pick best option
+3. Continue
+*/
+ 
+ll gcdll(ll a,ll b){
+    return __gcd(a,b);
+}
+ 
+ll lcmll(ll a,ll b){
+    return (a/gcdll(a,b))*b;
+}
+ 
+ll binpow(ll a,ll b){
+    ll res=1;
+    while(b){
+        if(b&1) res*=a;
+        a*=a;
+        b>>=1;
+    }
+    return res;
+}
+ 
+void solve(){
+     int n;
+     string s;
+     cin >> n >> s;
+     string ans = "";
+     int mx = 0;
+     for(int i=0;i<n-1 ; i++){
+        int count =0;
+        for(int j=0;j<n-1;j++){
+        if(s[i]==s[j] && s[i+1]==s[j+1]){
+            count++;
+        }    
+        }
+        if(count > mx){
+            mx=count;
+            ans="";
+            ans=ans + s[i];
+            ans = ans + s[i+1];
+        }
+     } cout << ans;
+}
+ 
+int main(){
+    fastIO();
+ 
+    //int t;
+    //cin>>t;
+ 
+    //while(t--){
+        solve();
+ 
+    return 0;
 }
